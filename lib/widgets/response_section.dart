e411e91cfaeb0b38a9387568419bd8e797e9fc81
Varsha Rana <ranavarsha321@gmail.com>
2026-08-31 22:44:35 +0530

    Added Skeleton to Sources and Response in Chat Page

--- lib/widgets/response_section.dart
+++ lib/widgets/response_section.dart
@@ -1,6 +1,7 @@
 import 'package:curio/services/chat_web_service.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
+import 'package:skeletonizer/skeletonizer.dart';
 
 class ResponseSection extends StatefulWidget {
   @override
@@ -8,14 +9,47 @@ class ResponseSection extends StatefulWidget {
 }
 
 class _ResponseSectionState extends State<ResponseSection> {
-  String response = "";
+  bool isLoading = true;
+  String response = """
+  ## Lists
+
+Unordered
+
++ Create a list by starting a line with `+`, `-`, or `*`
++ Sub-lists are made by indenting 2 spaces:
+  - Marker character change forces new list start:
+    * Ac tristique libero volutpat at
+    + Facilisis in pretium nisl aliquet
+    - Nulla volutpat aliquam velit
++ Very easy!
+
+Ordered
+
+1. Lorem ipsum dolor sit amet
+2. Consectetur adipiscing elit
+3. Integer molestie lorem at massa
+
+
+1. You can use sequential numbers...
+1. ...or keep all the numbers as `1.`
+
+Start numbering with offset:
+
+57. foo
+1. bar
+
+  """;
 
   @override
   void initState() {
     super.initState();
     ChatWebService().contentStream.listen((data) {
+      if (isLoading) {
+        response = "";
+      }
       setState(() {
         response += data['data'];
+        isLoading = false;
       });
     });
   }
@@ -42,34 +76,37 @@ class _ResponseSectionState extends State<ResponseSection> {
 
         const Divider(height: 24, color: Color(0xFFF3E8FF)),
 
-        MarkdownBody(
-          data: response.isEmpty ? "Thinking..." : response,
-          shrinkWrap: true,
-          styleSheet: MarkdownStyleSheet(
-            p: const TextStyle(
-              fontSize: 15,
-              height: 1.6,
-              color: Colors.black87,
-            ),
-            h1: const TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-              color: Color(0xFF7C3AED),
-            ),
-            h2: const TextStyle(
-              fontSize: 18,
-              fontWeight: FontWeight.bold,
-              color: Color(0xFF7C3AED),
-            ),
-            code: TextStyle(
-              backgroundColor: const Color(0xFFF3E8FF).withOpacity(0.5),
-              color: const Color(0xFF6D28D9),
-              fontSize: 14,
-            ),
-            codeblockDecoration: BoxDecoration(
-              color: const Color(0xFFF8F6FF),
-              borderRadius: BorderRadius.circular(12),
-              border: Border.all(color: const Color(0xFFE9D5FF)),
+        Skeletonizer(
+          enabled: isLoading,
+          child: MarkdownBody(
+            data: response.isEmpty ? "Thinking..." : response,
+            shrinkWrap: true,
+            styleSheet: MarkdownStyleSheet(
+              p: const TextStyle(
+                fontSize: 15,
+                height: 1.6,
+                color: Colors.black87,
+              ),
+              h1: const TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: Color(0xFF7C3AED),
+              ),
+              h2: const TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+                color: Color(0xFF7C3AED),
+              ),
+              code: TextStyle(
+                backgroundColor: const Color(0xFFF3E8FF).withOpacity(0.5),
+                color: const Color(0xFF6D28D9),
+                fontSize: 14,
+              ),
+              codeblockDecoration: BoxDecoration(
+                color: const Color(0xFFF8F6FF),
+                borderRadius: BorderRadius.circular(12),
+                border: Border.all(color: const Color(0xFFE9D5FF)),
+              ),
             ),
           ),
         ),
--- lib/widgets/sources_section.dart
+++ lib/widgets/sources_section.dart
@@ -1,5 +1,6 @@
 import 'package:curio/services/chat_web_service.dart';
 import 'package:flutter/material.dart';
+import 'package:skeletonizer/skeletonizer.dart';
 
 class SourceSection extends StatefulWidget {
   @override
@@ -7,7 +8,18 @@ class SourceSection extends StatefulWidget {
 }
 
 class _SourceSectionState extends State<SourceSection> {
-  List sources = [];
+  bool isLoading = true;
+  List sources = [
+    {
+      'title': 'Mr Elon Musk FRS',
+      'url': 'https://royalsociety.org/people/elon-musk-13829/',
+    },
+    {'title': 'Elon Musk', 'url': 'https://en.wikipedia.org/wiki/Elon_Musk'},
+    {
+      'title': 'Elon Musk : The Owner',
+      'url': 'https://www.tesla.com/elon-musk',
+    },
+  ];
 
   @override
   void initState() {
@@ -15,6 +27,7 @@ class _SourceSectionState extends State<SourceSection> {
     ChatWebService().searchResultStream.listen((data) {
       setState(() {
         sources = data['data'];
+        isLoading = false;
       });
     });
   }
@@ -28,50 +41,53 @@ class _SourceSectionState extends State<SourceSection> {
         itemCount: sources.length,
         itemBuilder: (context, index) {
           final item = sources[index];
-          return Container(
-            width: 150,
-            margin: const EdgeInsets.only(right: 10),
-            padding: const EdgeInsets.all(12),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(16),
-              border: Border.all(color: const Color(0xFFF3E8FF)),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.purple.withOpacity(0.04),
-                  blurRadius: 10,
-                  offset: const Offset(0, 4),
-                ),
-              ],
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  "[${index + 1}] ${item['title'] ?? 'Source'}",
-                  maxLines: 1,
-                  overflow: TextOverflow.ellipsis,
-                  style: const TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 13,
-                    color: Color(0xFF7C3AED),
+          return Skeletonizer(
+            enabled: isLoading,
+            child: Container(
+              width: 150,
+              margin: const EdgeInsets.only(right: 10),
+              padding: const EdgeInsets.all(12),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(16),
+                border: Border.all(color: const Color(0xFFF3E8FF)),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.purple.withOpacity(0.04),
+                    blurRadius: 10,
+                    offset: const Offset(0, 4),
                   ),
-                ),
-
-                const SizedBox(height: 4),
-
-                Text(
-                  item['url'] ?? '',
-                  maxLines: 1,
-                  overflow: TextOverflow.ellipsis,
-                  style: const TextStyle(
-                    fontSize: 11,
-                    color: Colors.black45,
-                    fontWeight: FontWeight.w700,
+                ],
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "[${index + 1}] ${item['title'] ?? 'Source'}",
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 13,
+                      color: Color(0xFF7C3AED),
+                    ),
                   ),
-                ),
-              ],
+
+                  const SizedBox(height: 4),
+
+                  Text(
+                    item['url'] ?? '',
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                    style: const TextStyle(
+                      fontSize: 11,
+                      color: Colors.black45,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                ],
+              ),
             ),
           );
         },
